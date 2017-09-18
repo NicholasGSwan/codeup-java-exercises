c30a1ec6c99dbcb980dee3144dc05076bd13e32f
Uglygrayduck <Nicholasgswan@gmail.com>
2017-09-18 15:21:34 -0500

    complete java syntax exercises

---
+++ src/JavaSyntax.java
@@ -0,0 +1,26 @@
+public class JavaSyntax {
+    public static void main(String[] args){
+        int myFavoriteNumber = 1200;
+        System.out.println(myFavoriteNumber);
+        String myString = "This is my string.";
+        System.out.println(myString);
+        float myNumber;
+        myNumber = 3.14f;
+        System.out.println(myNumber);
+        int x = 4;
+        x +=5;
+        System.out.println(x);
+
+        int d = 3;
+        int l = 4;
+        l *= d;
+        System.out.println(l);
+
+        int f = 10;
+        int g = 2;
+        f /= g;
+        g -= f;
+        System.out.println(g);
+        System.out.println(f);
+    }
+}
