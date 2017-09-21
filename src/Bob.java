08333319c2324efc411fef50fba72cd22c26ab68
Uglygrayduck <Nicholasgswan@gmail.com>
2017-09-21 09:27:54 -0500

    complete Bob exercise

---
+++ src/Bob.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String userInput;
+        String BobQuestionResponse = "Sure";
+        String BobYellResponse = "Whoa, chill out!";
+        String BobIgnoreResponse = "Fine, Be that way!";
+        String BobAnythingElse = "Whatever.";
+
+        while (true){
+            System.out.println("Bob looks in your direction, but you're pretty sure he's not really looking at you.\n What do you say?");
+            userInput = sc.nextLine();
+            if (userInput.endsWith("?")){
+                System.out.println(BobQuestionResponse);
+            }else if(userInput.endsWith("!")){
+                System.out.println(BobYellResponse);
+            }else if (userInput.isEmpty()){
+                System.out.println(BobIgnoreResponse);
+            }else {
+                System.out.println(BobAnythingElse);
+            }
+            System.out.println("If you would like to leave, type \"leave\" or \"quit\".");
+            if(userInput.equalsIgnoreCase("leave")||userInput.equalsIgnoreCase("quit")) {
+                break;
+            }
+            }
+        }
+    }
