bf9f3b8a1493e430f3e86bf572051b55399350ef
Uglygrayduck <Nicholasgswan@gmail.com>
2017-09-28 10:37:04 -0500

    -add default constructor to Rectangle class -create Square class -create Square class constructor using Rectangle constructor

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -3,10 +3,19 @@ package shapes;
 public class Rectangle {
     private int length;
     private int width;
+    public Rectangle(){
+
+    }
 
     public Rectangle(int length, int width){
         this.length = length;
         this.width = width;
+    }
+    public void setLength(int length){
+        this.length=length;
+    }
+    public void setWidth(int width){
+        this.width=width;
     }
 
     public int getPerimeter(){
---
+++ src/shapes/Square.java
@@ -0,0 +1,7 @@
+package shapes;
+
+public class Square extends Rectangle {
+    public Square(int side){
+        new Rectangle(side, side);
+    }
+}
