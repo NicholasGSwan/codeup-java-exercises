0f707f31ffad1c86de6d05ad8fa2b6c53d093929
Uglygrayduck <Nicholasgswan@gmail.com>
2017-09-28 13:36:59 -0500

    corrected a typo

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -8,11 +8,11 @@ public class ShapesTest {
 
         System.out.println(box1.getArea()+"\n"+box1.getPerimeter());
         System.out.println(box2.getArea()+"\n"+box2.getPerimeter());
-        showRectanglInfo(box1);
-        showRectanglInfo(box2);
-        showRectanglInfo(box3);
+        showRectangleInfo(box1);
+        showRectangleInfo(box2);
+        showRectangleInfo(box3);
     }
-    public static void showRectanglInfo(Rectangle rectangle){
+    public static void showRectangleInfo(Rectangle rectangle){
         System.out.println("The area is "+rectangle.getArea()+", and the perimeter is "+rectangle.getPerimeter()+".");
     }
 }
